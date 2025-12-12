4fdf840d63252ac36a8bad51a2afdd0c1f72c64f
Bikram <bikramadityadas1@gmail.com>
2025-12-12 18:46:52 +0530

    Test Trigger

--- tests/sample.spec.ts
+++ tests/sample.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('basic navigation test', async ({ page }) => {
   await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle(/Playwright/);
-  await page.click('text=Get started');
+  
 });
